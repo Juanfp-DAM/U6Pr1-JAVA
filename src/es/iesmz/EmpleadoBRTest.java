f6b4c3bf5946c3faf3a58703c878beab82596b91
JuanFP <juaferpui@alu.edu.gva.es>
2024-03-05 13:42:05 +0100

    SalarioBruto1a11 (TODOS)

--- src/es/iesmz/EmpleadoBRTest.java
+++ src/es/iesmz/EmpleadoBRTest.java
@@ -43,4 +43,45 @@ class EmpleadoBRTest {
         assertEquals(resultaodEsperado,resultadoReal,0.01);
     }
 
+    @Test
+    public void testCalculaSalarioBruto6(){
+        float resultadoReal=EmpleadoBR.calculaSalarioBruto(TipoEmpleado.encargado,999.99f,3);
+        float resultaodEsperado=1560;
+        assertEquals(resultaodEsperado,resultadoReal,0.01);
+    }
+
+    @Test
+    public void testCalculaSalarioBruto7(){
+        float resultadoReal=EmpleadoBR.calculaSalarioBruto(TipoEmpleado.encargado,500.f,0);
+        float resultaodEsperado=1500;
+        assertEquals(resultaodEsperado,resultadoReal,0.01);
+    }
+
+    @Test
+    public void testCalculaSalarioBruto8(){
+        float resultadoReal=EmpleadoBR.calculaSalarioBruto(TipoEmpleado.encargado,0,8);
+        float resultaodEsperado=1660;
+        assertEquals(resultaodEsperado,resultadoReal,0.01);
+    }
+
+    @Test
+    public void testCalculaSalarioBruto9(){
+        float resultadoReal=EmpleadoBR.calculaSalarioBruto(TipoEmpleado.encargado,-1,8);
+        float resultaodEsperado=-1;
+        assertEquals(resultaodEsperado,resultadoReal,0.01);
+    }
+
+    @Test
+    public void testCalculaSalarioBruto10(){
+        float resultadoReal=EmpleadoBR.calculaSalarioBruto(TipoEmpleado.encargado,1500,-1);
+        float resultaodEsperado=-1;
+        assertEquals(resultaodEsperado,resultadoReal,0.01);
+    }
+
+    @Test
+    public void testCalculaSalarioBruto11(){
+        float resultadoReal=EmpleadoBR.calculaSalarioBruto(null,1500,8);
+        float resultaodEsperado=-1;
+        assertEquals(resultaodEsperado,resultadoReal,0.01);
+    }
 }
